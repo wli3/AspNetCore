e4198e864516e9692c1733af50b419fdcdf697b2
Ryan Nowak <nowakra@gmail.com>
2019-02-28 15:57:51 -0800

    Add fallback routing for controllers and pages

---
+++ src/Mvc/Mvc.Core/src/Routing/DynamicControllerEndpointSelector.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Routing;
+
+namespace Microsoft.AspNetCore.Mvc.Routing
+{
+    internal class DynamicControllerEndpointSelector : IDisposable
+    {
+        private readonly ControllerActionEndpointDataSource _dataSource;
+        private readonly DataSourceDependentCache<ActionSelectionTable<RouteEndpoint>> _cache;
+
+        public DynamicControllerEndpointSelector(ControllerActionEndpointDataSource dataSource)
+        {
+            if (dataSource == null)
+            {
+                throw new ArgumentNullException(nameof(dataSource));
+            }
+
+            _dataSource = dataSource;
+            _cache = new DataSourceDependentCache<ActionSelectionTable<RouteEndpoint>>(dataSource, Initialize);
+        }
+
+        private ActionSelectionTable<RouteEndpoint> Table => _cache.EnsureInitialized();
+
+        // This is async because the page version will be async. We don't want to put ourselves in a
+        // position where these types become public and then users start using them in sync locations.
+        public Task<IReadOnlyList<RouteEndpoint>> SelectEndpointsAsync(RouteValueDictionary values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            var table = Table;
+            var matches = table.Select(values);
+            return Task.FromResult(matches);
+        }
+
+        private static ActionSelectionTable<RouteEndpoint> Initialize(IReadOnlyList<Endpoint> endpoints)
+        {
+            return ActionSelectionTable<RouteEndpoint>.Create(endpoints);
+        }
+
+        public void Dispose()
+        {
+            _cache.Dispose();
+        }
+    }
+}
